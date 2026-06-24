e0d93bc79bcee3ad3df7419ab9773e3924dd0ea6
K3nnutCode <151482756+K3nnutCode@users.noreply.github.com>
2026-06-24 10:49:29 +0800

    Add service worker for caching assets

---
+++ sw.js
@@ -0,0 +1,22 @@
+const CACHE_NAME = 'gacha-v1';
+
+self.addEventListener('install', (e) => {
+  e.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      return cache.addAll([
+        '/gacha-schedule/',
+        '/gacha-schedule/index.html',
+        '/gacha-schedule/manifest.json'
+      ]);
+    })
+  );
+  self.skipWaiting();
+});
+
+self.addEventListener('fetch', (e) => {
+  e.respondWith(
+    caches.match(e.request).then((response) => {
+      return response || fetch(e.request);
+    })
+  );
+});
